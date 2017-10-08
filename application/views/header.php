2152515e5239550d900a60d620ed678944f758dc
sizijay <siziroxz@gmail.com>
2017-10-08 12:22:06 +0530

    integrate home_view v1.0

---
+++ application/views/header.php
@@ -0,0 +1,34 @@
+<html>
+	<body>
+		<!-- header -->
+                 <div class="header-inner">
+					  <h1 class="logo">
+						<a href="index.html"><span><font size="2">Department of Physical Education</font></span>University of Colombo</a></h1>
+						<nav class="navbar navbar-default">
+							<div class="navbar-header">
+								<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+									<span class="sr-only">Toggle navigation</span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+							<!-- navbar-header -->
+							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+								<ul class="nav navbar-nav navbar-right">
+									<li><a href="#" data-anchor="active">Home</a></li>
+									<li><a href="#" data-anchor="about">About</a></li>
+									<li><a href="#" data-anchor="classes">Trainning Programes</a></li>
+									<li><a href="#" data-anchor="clients">Nutrition Plans</a></li>
+									<li><a href="#" data-anchor="gallery">Reviews</a></li>
+									<li><a href="#" data-anchor="contact" >Contact</a></li>
+									<li><a href="#" data-anchor="contact" >Login</a></li>
+								</ul>
+							</div>
+							<div class="clearfix"> </div>	
+						</nav>
+						<div class="clearfix"></div>
+				</div>
+			<!-- //header -->
+	</body>
+</html>
