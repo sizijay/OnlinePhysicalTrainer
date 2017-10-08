3e69adb37df1c2724b7f448f89062ef3d14ca7da
UddeepaWijerathna <wijerathnauddeepa@gmail.com>
2017-10-08 21:46:00 +0530

    contact

---
+++ application/views/contact_view.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>About Us
+</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Gym Workout Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="<?php echo base_url();?>/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url();?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/cm-overlay.css" /><!-- //overlay-->
+<link rel="stylesheet" href="<?php echo base_url();?>/css/owl.carousel.css" type="text/css" media="all"/> <!-- Owl-Carousel-CSS -->
+<link href="<?php echo base_url();?>css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<?php include "header.php" ?>
+<body>
+<div class="contact-w3layouts">
+<div class="container">
+<div class="col-md-6 contact-agileits-w3layouts-left">
+				<h3 class="title-w3-agile-sub">Contact <span>Us</span></h3>
+				<p class="para-agileits-w3layouts">Malesuada penatibus felis Neque porro quisquam est qui dolorem Lorem ipsum dolor sit amet, aptent neque fringilla duis erast ridiculus.</p>
+				<p class="sub">California St San Francisco, CA USA<span class="glyphicon glyphicon-map-marker icon" aria-hidden="true"></span></p>
+				<p class="sub">Mon-Fri : 9am - 10pm.  Sat : 9am - 12pm<span class="glyphicon glyphicon-time icon" aria-hidden="true"></span></p>
+				<p class="add"><span>Tel :</span> +0 (020) 123-5338<span class="glyphicon glyphicon glyphicon-earphone icon" aria-hidden="true"></span></p>
+				<p class="add"><span>Email :</span> <a class="mail" href="mailto:mail@example.com">info(at)example.com</a><span class="glyphicon glyphicon-envelope icon" aria-hidden="true"></span></p>
+
+			</div>
+			<div class="col-md-6 mail-grid1-form ">
+				<h3 class="title-w3-agile-sub">Send a <span>Message</span></h3>
+				<form action="#" method="post">
+					<input type="text" name="Name" placeholder="Name" required="" />
+					<input type="email" name="Email" placeholder="Email" required="" />
+					<textarea name="Message" placeholder="Type Your Text Here...." required=""></textarea>
+					<input type="submit" value="Submit" />
+				</form>
+			</div>
+			<div class="clearfix"></div>
+</div>
+</div>
+
+
+
+
+</body>
+<?php include "footer.php" ?>
+</html>
