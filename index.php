b7cedb99c679392a8362e0f16283a06fcd9b46b6
sizijay <siziroxz@gmail.com>
2017-10-22 23:57:54 +0530

    Revert "html part 2"
    This reverts commit 9f538292f1d417b2dc3359b4c90b7787ff1f3396.

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-	    <title>Buyer</title>
-
-	    <!-- Bootstrap core CSS -->
-	    <link href="css/bootstrap.min.css" rel="stylesheet">
-		
-		<link href="css/homepage.css" rel="stylesheet">
-
-	  </head>
-
-	  <body>
 <?php
 /**
  * CodeIgniter
@@ -329,5 +315,4 @@ switch (ENVIRONMENT)
  * And away we go...
  */
 require_once BASEPATH.'core/CodeIgniter.php';
-</body>
 </html>
