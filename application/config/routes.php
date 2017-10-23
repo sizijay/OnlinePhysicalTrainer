092f46c2bc578e7193fbb6e9e6c233cf32c7192a
sizijay <siziroxz@gmail.com>
2017-10-23 17:40:30 +0530

    check 4

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Home';
+$route['default_controller'] = 'home';
 
-$route['home'] = 'home';
+#$route['home'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
