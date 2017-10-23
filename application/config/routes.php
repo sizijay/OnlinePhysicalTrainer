93df6df464547202a01d1dabaebfc37494307416
sizijay <siziroxz@gmail.com>
2017-10-23 17:55:22 +0530

    r2

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'about';
 
-$route['home'] = 'home';
+
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
