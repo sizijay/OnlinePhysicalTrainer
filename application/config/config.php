cd65ce2a2ca2d55a26e8608b0983d9bf763a9553
sizijay <siziroxz@gmail.com>
2017-10-23 17:27:53 +0530

    check2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://ec2-54-70-176-254.us-west-2.compute.amazonaws.com/OnlinePhysicalTrainer';
 
 /*
 |--------------------------------------------------------------------------
