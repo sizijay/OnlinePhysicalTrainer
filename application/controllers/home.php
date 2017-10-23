44d7c6ae5eda7822c09aec0dc7a0f44069d2dc2e
sizijay <siziroxz@gmail.com>
2017-10-23 17:33:42 +0530

    check3

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,7 +3,7 @@ if (! Defined ('BASEPATH'))
 	
 exit ('No direct script access allowed');
 
-Class home extends CI_Controller {
+Class Home extends CI_Controller {
 	public function index(){
 		$this->load->view('home_view');
 	}
