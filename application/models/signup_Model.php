bd83949d050c6f744ff33b2af2692af2faa730bc
Piumi Lokuge <piumi1993indeewari@gmail.com>
2017-10-26 20:28:33 +0530

    signup_model and signup Controller

---
+++ application/models/signup_Model.php
@@ -0,0 +1,31 @@
+<?php 
+
+Class signup_Model extends CI_Model {
+	function insertUserdata(){
+		$dataL = array(
+
+			'NIC' => $this->input->post('nic',TRUE),
+			'fname' => $this->input->post('fname',TRUE),
+			'lname' => $this->input->post('lname',TRUE),						
+			'contactno' => $this->input->post('cnumber',TRUE),
+			'address' => $this->input->post('address',TRUE),
+			'email' => $this->input->post('email',TRUE),
+			'password' => sha1($this->input->post('password',TRUE))
+			);
+
+		$this->db->insert('login',$dataL);
+
+		$dataS = array(
+			'NIC' => $this->input->post('nic',TRUE),
+			'weight' => $this->input->post('weight',TRUE),
+			'height' => $this->input->post('height',TRUE),
+			'age' => $this->input->post('age',TRUE),
+			);
+		
+		$this->db->insert('student',$dataS);
+
+
+	}
+}
+
+?>
