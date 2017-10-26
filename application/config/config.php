f7a4ab029383ef156182240b3e1676252734ada8
sizijay <siziroxz@gmail.com>
2017-10-26 16:38:34 +0530

    config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,12 +23,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-<<<<<<< Updated upstream
-$config['base_url'] = 'http://localhost:/OnlinePhysicalTrainer';
-=======
-$config['base_url'] = 'http://localhost:8888/OnlinePhysicalTrainer';
-//$config['base_url'] = 'http://ec2-54-70-176-254.us-west-2.compute.amazonaws.com/OnlinePhysicalTrainer';
->>>>>>> Stashed changes
+
+
+//$config['base_url'] = 'http://localhost:8888/OnlinePhysicalTrainer';
+$config['base_url'] = 'http://ec2-54-70-176-254.us-west-2.compute.amazonaws.com/OnlinePhysicalTrainer';
+
 
 /*
 |--------------------------------------------------------------------------
