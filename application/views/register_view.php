a2e5a9f1d28161df2275c4dc2d68a9e675f620cb
sizijay <siziroxz@gmail.com>
2017-10-26 22:34:07 +0530

    Registration

---
+++ application/views/register_view.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Login
+</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Gym Workout Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+    function hideURLbar(){ window.scrollTo(0,1); } </script>
+
+<link href="<?php echo base_url();?>css/styleSignup.css" rel="stylesheet" type="text/css" media="all" />
+<!-- js -->
+<script type="<?php echo base_url();?>text/javascript" src="js/jquery-2.1.4.minSignup.js"></script>
+<!-- //js -->
+<!-- //for-mobile-apps -->
+<link href="<?php echo base_url();?>/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url();?>/css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link type="text/css" rel="stylesheet" href="<?php echo base_url();?>css/cm-overlay.css" /><!-- //overlay-->
+<link rel="stylesheet" href="<?php echo base_url();?>/css/owl.carousel.css" type="text/css" media="all"/> <!-- Owl-Carousel-CSS -->
+<link href="<?php echo base_url();?>css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url();?>css/login.css" rel="stylesheet" type="text/css" media="all" />
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900" rel="stylesheet">
+<!--//fonts-->
+
+<!-- font-awesome-icons -->
+<!-- //font-awesome-icons -->
+<link href="//fonts.googleapis.com/css?family=Anton&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+</head>
+<?php include "header.php" ?>
+<body>
+<div id="single">
+<table height="100%" width="100%" cellpadding="0" cellspacing="0" border="0">
+   <tr>
+     <td valign="top" align="left" background="http://liwli.com/wp-content/uploads/2015/05/gym-workout.jpg" style="background-repeat:no-repeat; background-size:cover;"> 
+<!-- banner -->
+  <div class="center-container">
+  <div class="banner-dott">
+    <div class="main">
+      <h1 class="w3layouts_head">Registration Form</h1>
+        <div class="w3layouts_main_grid">
+          <form action="#" method="post" class="w3_form_post">
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Name </label>
+                <input type="text" id= "name" name="Task" placeholder="Name" required="">
+              </span>
+            </div>
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Age </label>
+                <input type="text" id= "age" name="Task" placeholder="Age" required="">
+              </span>
+            </div>
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Email </label>
+                <input type="text" id= "email" name="Task" placeholder="Email" required="">
+              </span>
+            </div>
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Phone Number </label>
+                <input type="text" id= "phoneNumber" name="Task" placeholder="Phone Number" required="">
+                </span>
+            </div>
+
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Address </label>
+                <input type="text" id= "address" name="Task" placeholder="Address" required="">
+                </span>
+            </div>
+
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Height </label>
+                <input type="text" id= "height" name="Task" placeholder="Height" required="">
+                </span>
+            </div>
+
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Weight </label>
+                <input type="text" id= "weight" name="Task" placeholder="Weight" required="">
+                </span>
+            </div>
+
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Faculty </label>
+                <select name="country">
+                  <option value="none" selected="" disabled="">Select Option</option>
+                  <option value="Ph">UCSC</option>
+                  <option value="Ph">Science</option>
+                  <option value="Po">Art</option>
+                  <option value="So">Management</option>                
+                </select>
+              </span>
+            </div>
+
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Reg No </label>
+                <input type="text" id= "weight" name="Task" placeholder="Reg No" required="">
+                </span>
+            </div>
+
+            <div class="w3_agileits_main_grid w3l_main_grid">
+              <span class="agileits_grid">
+                <label>Select Category </label>
+                <select name="country">
+                  <option value="none" selected="" disabled="">Select Category</option>
+                  <option value="Ph">Aerobic</option>
+                  <option value="Po">Strength Exercises</option>
+                  <option value="So">Flexibility</option>
+                  <option value="Sp">Balance Exercises</option>
+
+                </select>
+              </span>
+            </div>
+            
+          
+          <div class="w3_main_grid">
+            <div class="w3_main_grid_right">
+              <input type="submit" value="Submit">
+            </div>
+          </div>
+        </form>
+      </div>
+    <!-- Calendar -->
+      <link rel="stylesheet" href="css/jquery-uiSignup.css" />
+        <script src="js/jquery-ui.js"></script>
+          <script>
+            $(function() {
+              $( "#datepicker" ).datepicker();
+            });
+          </script>
+    <!-- //Calendar -->
+      <div class="w3layouts_copy_right">
+        <div class="container">
+          <p>© 2017 UoC Gym Registration Form. All rights reserved | Design by UCSC</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+<?php include "footer.php" ?>
+</div>
+</td>
+   </tr>
+ </table>
+
+</body>
+
+
+</html>
